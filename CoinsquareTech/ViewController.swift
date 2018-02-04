3b8fd209660702de7c742bb2bbe259067a6238a7
victorhong1988 <victorhong1988@gmail.com>
2018-02-03 19:46:17 -0500

    Initial Commit

---
+++ CoinsquareTech/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoinsquareTech
+//
+//  Created by Victor Hong on 03/02/2018.
+//  Copyright © 2018 Victor Hong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
